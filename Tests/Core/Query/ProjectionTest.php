181a3d2791b9d1c4eddbafee13018647c209a973
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-05-15 17:43:36 +0200

    added basic projection implementation

---
+++ Tests/Core/Query/ProjectionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Momm\Tests\Core\Query;
+
+use Momm\Core\Query\Projection;
+
+class ProjectionTest extends \PHPUnit_Framework_TestCase
+{
+    use SqlTestTrait;
+
+    public function testProjection()
+    {
+        $projection = (new Projection())
+            ->setField('foo')
+            ->setField('bar', null, 'int4')
+            ->setField('baz', 'cassoulet', 'varchar')
+            ->setField('roger', 'tabouret')
+        ;
+
+        $reference = <<<EOT
+foo as foo,
+bar::int4 as bar,
+cassoulet::varchar as baz,
+tabouret as roger
+EOT;
+
+        // Remove all whitespaces, and lowercase everything to ensure our
+        // generation cruft don't change trigger false negatives
+        $this->assertSameSql($reference, (string)$projection);
+    }
+}
