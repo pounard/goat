bb93b7e4b93b2894ead4b364445260caa37d6d36
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-01-15 14:18:20 +0100

    moved default conversion type mappings to ConvertMap::getDefautConverterMap()

--- Core/Converter/ConverterMap.php
+++ Core/Converter/ConverterMap.php
@@ -2,13 +2,93 @@
 
 namespace Goat\Core\Converter;
 
+use Goat\Core\Converter\Impl\BooleanConverter;
+use Goat\Core\Converter\Impl\DecimalConverter;
+use Goat\Core\Converter\Impl\IntegerConverter;
 use Goat\Core\Converter\Impl\StringConverter;
+use Goat\Core\Converter\Impl\TimestampConverter;
 use Goat\Core\DebuggableTrait;
 use Goat\Core\Error\ConfigurationError;
 
+/**
+ * Converter map contains references to all existing converters and is the
+ * central point of all native to SQL or SQL to native type conversion.
+ */
 class ConverterMap implements ConverterInterface
 {
     use DebuggableTrait;
+
+    /**
+     * Get default converter map
+     *
+     * @return array
+     *   Keys are type identifiers, values are arrays containing:
+     *     - first value is the converter class name
+     *     - second value is a type aliases array
+     */
+    static public function getDefautConverterMap() : array
+    {
+        /*
+         * Mapping from PostgreSQL 9.2
+         *
+        bigint 	int8 	signed eight-byte integer
+        bigserial 	serial8 	autoincrementing eight-byte integer
+        ## bit [ (n) ] 	  	fixed-length bit string
+        ## bit varying [ (n) ] 	varbit 	variable-length bit string
+        boolean 	bool 	logical Boolean (true/false)
+        ## box 	  	rectangular box on a plane
+        bytea 	  	binary data ("byte array")
+        character [ (n) ] 	char [ (n) ] 	fixed-length character string
+        character varying [ (n) ] 	varchar [ (n) ] 	variable-length character string
+        ## cidr 	  	IPv4 or IPv6 network address
+        ## circle 	  	circle on a plane
+        date 	  	calendar date (year, month, day)
+        double precision 	float8 	double precision floating-point number (8 bytes)
+        ## inet 	  	IPv4 or IPv6 host address
+        integer 	int, int4 	signed four-byte integer
+        ## interval [ fields ] [ (p) ] 	  	time span
+        ## json 	  	JSON data
+        ## line 	  	infinite line on a plane
+        ## lseg 	  	line segment on a plane
+        ## macaddr 	  	MAC (Media Access Control) address
+        ## money 	  	currency amount
+        numeric [ (p, s) ] 	decimal [ (p, s) ] 	exact numeric of selectable precision
+        ## path 	  	geometric path on a plane
+        ## point 	  	geometric point on a plane
+        ## polygon 	  	closed geometric path on a plane
+        real 	float4 	single precision floating-point number (4 bytes)
+        smallint 	int2 	signed two-byte integer
+        smallserial 	serial2 	autoincrementing two-byte integer
+        serial 	serial4 	autoincrementing four-byte integer
+        text 	  	variable-length character string
+        time [ (p) ] [ without time zone ] 	  	time of day (no time zone)
+        time [ (p) ] with time zone 	timetz 	time of day, including time zone
+        timestamp [ (p) ] [ without time zone ] 	  	date and time (no time zone)
+        timestamp [ (p) ] with time zone 	timestamptz 	date and time, including time zone
+        ## tsquery 	  	text search query
+        ## tsvector 	  	text search document
+        ## txid_snapshot 	  	user-level transaction ID snapshot
+        ## uuid 	  	universally unique identifier
+        ## xml 	  	XML data
+         */
+
+        return [
+            'timestampz' => [TimestampConverter::class, ['timestamp', 'datetime']],
+            'date' => [TimestampConverter::class, []],
+            'timez' => [TimestampConverter::class, ['time']],
+            'varchar' => [StringConverter::class, ['character', 'char']],
+            'bytea' => [StringConverter::class, ['blob']],
+            'boolean' => [BooleanConverter::class, ['bool']],
+            'bigint' => [IntegerConverter::class, ['int8']],
+            'bigserial' => [IntegerConverter::class, ['serial8']],
+            'integer' => [IntegerConverter::class, ['int', 'int4']],
+            'serial' => [IntegerConverter::class, ['serial4']],
+            'smallint' => [IntegerConverter::class, ['int2', 'smallserial', 'serial2']],
+            'double' => [DecimalConverter::class, ['float8']],
+            'numeric' => [DecimalConverter::class, ['decimal']],
+            'real' => [DecimalConverter::class, ['float4']],
+        ];
+    }
 
     /**
      * @var string[]
--- Tests/DriverTestCase.php
+++ Tests/DriverTestCase.php
@@ -6,11 +6,6 @@ use Goat\Core\Client\ConnectionInterface;
 use Goat\Core\Client\Dsn;
 use Goat\Core\Client\Session;
 use Goat\Core\Converter\ConverterMap;
-use Goat\Core\Converter\Impl\BooleanConverter;
-use Goat\Core\Converter\Impl\DecimalConverter;
-use Goat\Core\Converter\Impl\IntegerConverter;
-use Goat\Core\Converter\Impl\StringConverter;
-use Goat\Core\Converter\Impl\TimestampConverter;
 use Goat\Core\EventDispatcher\EventEmitterConnectionProxy;
 use Goat\Core\Hydrator\HydratorMap;
 
@@ -102,72 +97,15 @@ abstract class DriverTestCase extends \PHPUnit_Framework_TestCase
      */
     final protected function createConverter(ConnectionInterface $connection) : ConverterMap
     {
-        $default = new StringConverter();
+        $map = new ConverterMap();
 
-        /*
-         * Mapping from PostgreSQL 9.2
-         *
-        bigint 	int8 	signed eight-byte integer
-        bigserial 	serial8 	autoincrementing eight-byte integer
-        ## bit [ (n) ] 	  	fixed-length bit string
-        ## bit varying [ (n) ] 	varbit 	variable-length bit string
-        boolean 	bool 	logical Boolean (true/false)
-        ## box 	  	rectangular box on a plane
-        bytea 	  	binary data ("byte array")
-        character [ (n) ] 	char [ (n) ] 	fixed-length character string
-        character varying [ (n) ] 	varchar [ (n) ] 	variable-length character string
-        ## cidr 	  	IPv4 or IPv6 network address
-        ## circle 	  	circle on a plane
-        date 	  	calendar date (year, month, day)
-        double precision 	float8 	double precision floating-point number (8 bytes)
-        ## inet 	  	IPv4 or IPv6 host address
-        integer 	int, int4 	signed four-byte integer
-        ## interval [ fields ] [ (p) ] 	  	time span
-        ## json 	  	JSON data
-        ## line 	  	infinite line on a plane
-        ## lseg 	  	line segment on a plane
-        ## macaddr 	  	MAC (Media Access Control) address
-        ## money 	  	currency amount
-        numeric [ (p, s) ] 	decimal [ (p, s) ] 	exact numeric of selectable precision
-        ## path 	  	geometric path on a plane
-        ## point 	  	geometric point on a plane
-        ## polygon 	  	closed geometric path on a plane
-        real 	float4 	single precision floating-point number (4 bytes)
-        smallint 	int2 	signed two-byte integer
-        smallserial 	serial2 	autoincrementing two-byte integer
-        serial 	serial4 	autoincrementing four-byte integer
-        text 	  	variable-length character string
-        time [ (p) ] [ without time zone ] 	  	time of day (no time zone)
-        time [ (p) ] with time zone 	timetz 	time of day, including time zone
-        timestamp [ (p) ] [ without time zone ] 	  	date and time (no time zone)
-        timestamp [ (p) ] with time zone 	timestamptz 	date and time, including time zone
-        ## tsquery 	  	text search query
-        ## tsvector 	  	text search document
-        ## txid_snapshot 	  	user-level transaction ID snapshot
-        ## uuid 	  	universally unique identifier
-        ## xml 	  	XML data
-         */
+        foreach (ConverterMap::getDefautConverterMap() as $type => $data) {
+            list($class, $aliases) = $data;
 
-        // Order of converters and types gives you the order in which they will
-        // be guessed if no type is specified, go from the more complex to the
-        // lesser to ensure there is no data loss in such case.
-        return (new ConverterMap())
-            ->register('timestampz', new TimestampConverter(), ['timestamp', 'datetime'])
-            ->register('date', new TimestampConverter())
-            ->register('timez', new TimestampConverter(), ['time'])
-            ->register('varchar', $default, ['character', 'char'])
-            ->register('bytea', $default, ['blob']) // @todo
-            ->register('boolean', new BooleanConverter(), ['bool'])
-            ->register('bigint', new IntegerConverter(), ['int8'])
-            ->register('bigserial', new IntegerConverter(), ['serial8'])
-            ->register('integer', new IntegerConverter(), ['int', 'int4'])
-            ->register('serial', new IntegerConverter(), ['serial4'])
-            ->register('smallint', new IntegerConverter(), ['int2', 'smallserial', 'serial2'])
-            ->register('double', new DecimalConverter(), ['float8'])
-            ->register('numeric', new DecimalConverter(), ['decimal'])
-            ->register('real', new DecimalConverter(), ['float4'])
-            ->setFallback($default)
-        ;
+            $map->register($type, new $class(), $aliases);
+        }
+
+        return $map;
     }
 
     /**
